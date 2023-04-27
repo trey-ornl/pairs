1236c07905d29b36483e4ccaf1b5f0d562d365cf
James Buford White <twhite@tioga11.llnl.gov>
2023-04-27 16:10:29 -0700

    Added loop over stride between pairs.

--- main.cc
+++ main.cc
@@ -4,6 +4,16 @@
 #include <hip/hip_runtime.h>
 #include <mpi.h>
 
+void checkHip(const hipError_t err, const char *const file, const int line)
+{
+  if (err == hipSuccess) return;
+  fprintf(stderr,"HIP ERROR AT LINE %d OF FILE '%s': %s %s\n",line,file,hipGetErrorName(err),hipGetErrorString(err));
+  fflush(stderr);
+  exit(err);
+}
+
+#define CHECK(X) checkHip(X,__FILE__,__LINE__)
+
 int main(int argc, char **argv)
 {
   MPI_Init(&argc,&argv);
@@ -11,7 +21,11 @@ int main(int argc, char **argv)
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
   int size = 0;
   MPI_Comm_size(MPI_COMM_WORLD,&size);
-  if (size%2) MPI_Abort(MPI_COMM_WORLD,size);
+  if (size%2) {
+    if (rank == 0) fprintf(stderr,"%s: ERROR, number of MPI tasks must be even (unlike %d)\n",argv[0],size);
+    MPI_Finalize();
+    return size;
+  }
   const int half = size/2;
 
   int n = 128*1024*1024;
@@ -28,10 +42,10 @@ int main(int argc, char **argv)
   MPI_Bcast(&mem,1,MPI_CHAR,0,MPI_COMM_WORLD);
   const size_t bytes = size_t(n)*sizeof(long);
   if (rank == 0) {
-    if (mem == 'd') printf("Using hipMalloc\n");
-    else if (mem == 'h') printf("Using hipHostMalloc\n");
-    else printf("Using malloc\n");
-    printf("%d longs, %lu bytes, %d pairs\n",n,bytes,half);
+    if (mem == 'd') printf("Using hipMalloc");
+    else if (mem == 'h') printf("Using hipHostMalloc");
+    else printf("Using malloc");
+    printf(" with %d longs, %lu bytes, %d pairs\n",n,bytes,half);
     fflush(stdout);
   }
 
@@ -40,18 +54,18 @@ int main(int argc, char **argv)
   long *ping = nullptr;
   long *pong = nullptr;
   if (mem == 'd') {
-    hipHostMalloc(&hping,bytes);
-    hipHostMalloc(&hpong,bytes);
-    hipMalloc(&ping,bytes);
-    hipMalloc(&pong,bytes);
+    CHECK(hipHostMalloc(&hping,bytes));
+    CHECK(hipHostMalloc(&hpong,bytes));
+    CHECK(hipMalloc(&ping,bytes));
+    CHECK(hipMalloc(&pong,bytes));
     for (int i = 0; i < n; i ++) hping[i] = i;
+    CHECK(hipMemset(pong,0,bytes));
+    CHECK(hipMemcpy(ping,hping,bytes,hipMemcpyHostToDevice));
     memset(hpong,0,bytes);
-    hipMemcpy(ping,hping,bytes,hipMemcpyHostToDevice);
-    hipMemset(pong,0,bytes);
   } else {
     if (mem == 'h') {
-      hipHostMalloc(&ping,bytes);
-      hipHostMalloc(&pong,bytes);
+      CHECK(hipHostMalloc(&ping,bytes));
+      CHECK(hipHostMalloc(&pong,bytes));
     } else {
       ping = reinterpret_cast<long*>(malloc(bytes));
       pong = reinterpret_cast<long*>(malloc(bytes));
@@ -60,70 +74,75 @@ int main(int argc, char **argv)
     memset(pong,0,bytes);
   }
 
-  const bool lower = (rank < half);
-  const int partner = lower ? rank+half : rank-half;
   const int tag = 1;
-
   const double gb = double(1024)*double(1024)*double(1024);
   const double timeout = 1;
 
-  if (rank == 0) {
-    printf("Warmup...\n");
-    fflush(stdout);
-  }
-
   MPI_Request req;
 
-  MPI_Irecv(pong,n,MPI_LONG,partner,tag,MPI_COMM_WORLD,&req);
-  if (lower) {
-    MPI_Send(ping,n,MPI_LONG,partner,tag,MPI_COMM_WORLD);
-    MPI_Wait(&req,MPI_STATUS_IGNORE);
-  } else {
-    MPI_Wait(&req,MPI_STATUS_IGNORE);
-    MPI_Send(pong,n,MPI_LONG,partner,tag,MPI_COMM_WORLD);
-  }
+  for (int stride = half; stride > 0; stride--) if (size%(2*stride) == 0) {
 
-  for (int parts = 1; parts < n; parts += parts) {
-
-    if (mem == 'd') {
-      hipMemset(pong,0,bytes);
-      hipDeviceSynchronize();
-    }
-    else memset(pong,0,bytes);
-
-    const int count = n/parts;
-    const int end = parts*count;
-    if (!lower) MPI_Irecv(pong,count,MPI_LONG,partner,tag,MPI_COMM_WORLD,&req);
-    MPI_Barrier(MPI_COMM_WORLD);
-    const double before = MPI_Wtime();
-    for (int offset = 0; offset < end; offset += count) {
-      if (lower) {
-        MPI_Irecv(pong+offset,count,MPI_LONG,partner,tag,MPI_COMM_WORLD,&req);
-        MPI_Send(ping+offset,count,MPI_LONG,partner,tag,MPI_COMM_WORLD);
-        MPI_Wait(&req,MPI_STATUS_IGNORE);
-      } else {
-        MPI_Wait(&req,MPI_STATUS_IGNORE);
-        if (offset+count < end) MPI_Irecv(pong+offset+count,count,MPI_LONG,partner,tag,MPI_COMM_WORLD,&req);
-        MPI_Send(pong+offset,count,MPI_LONG,partner,tag,MPI_COMM_WORLD);
-      }
-    }
-    const double delta = MPI_Wtime()-before;
-    double elapsed = 0;
-    MPI_Allreduce(&delta,&elapsed,1,MPI_DOUBLE,MPI_MAX,MPI_COMM_WORLD);
+    const bool lower = ((rank/stride)%2 == 0);
+    const int partner = lower ? rank+stride : rank-stride;
+    assert(partner >= 0);
+    assert(partner < size);
+    assert(partner != rank);
     if (rank == 0) {
-      const double bw = 2.0*double(end)*double(sizeof(long))/(elapsed*gb);
-      const double rate = 2.0*double(parts)/elapsed;
-      printf("%d ping-pongs of %d longs: %g seconds, %g GB/s, %g message/s\n",parts,count,elapsed,bw,rate);
+      printf("\nWarmup for %d pairs of stride %d...\n",half,stride);
       fflush(stdout);
     }
 
-    if (mem == 'd') {
-      hipMemcpy(hpong,pong,bytes,hipMemcpyDeviceToHost);
-      for (int i = 0; i < end; i++) if (hping[i] != hpong[i]) MPI_Abort(MPI_COMM_WORLD,i);
+    MPI_Irecv(pong,n,MPI_LONG,partner,tag,MPI_COMM_WORLD,&req);
+    if (lower) {
+      MPI_Send(ping,n,MPI_LONG,partner,tag,MPI_COMM_WORLD);
+      MPI_Wait(&req,MPI_STATUS_IGNORE);
     } else {
-      for (int i = 0; i < end; i++) if (ping[i] != pong[i]) MPI_Abort(MPI_COMM_WORLD,i);
+      MPI_Wait(&req,MPI_STATUS_IGNORE);
+      MPI_Send(pong,n,MPI_LONG,partner,tag,MPI_COMM_WORLD);
     }
-    if (elapsed > timeout) break;
+
+    for (int parts = 1; parts < n; parts += parts) {
+
+      if (mem == 'd') {
+        CHECK(hipMemset(pong,0,bytes));
+        CHECK(hipDeviceSynchronize());
+      }
+      else memset(pong,0,bytes);
+
+      const int count = n/parts;
+      const int end = parts*count;
+      if (!lower) MPI_Irecv(pong,count,MPI_LONG,partner,tag,MPI_COMM_WORLD,&req);
+      MPI_Barrier(MPI_COMM_WORLD);
+      const double before = MPI_Wtime();
+      for (int offset = 0; offset < end; offset += count) {
+        if (lower) {
+          MPI_Irecv(pong+offset,count,MPI_LONG,partner,tag,MPI_COMM_WORLD,&req);
+          MPI_Send(ping+offset,count,MPI_LONG,partner,tag,MPI_COMM_WORLD);
+          MPI_Wait(&req,MPI_STATUS_IGNORE);
+        } else {
+          MPI_Wait(&req,MPI_STATUS_IGNORE);
+          if (offset+count < end) MPI_Irecv(pong+offset+count,count,MPI_LONG,partner,tag,MPI_COMM_WORLD,&req);
+          MPI_Send(pong+offset,count,MPI_LONG,partner,tag,MPI_COMM_WORLD);
+        }
+      }
+      const double delta = MPI_Wtime()-before;
+      double elapsed = 0;
+      MPI_Allreduce(&delta,&elapsed,1,MPI_DOUBLE,MPI_MAX,MPI_COMM_WORLD);
+      if (rank == 0) {
+        const double bw = 2.0*double(end)*double(sizeof(long))/(elapsed*gb);
+        const double rate = 2.0*double(parts)/elapsed;
+        printf("%d ping-pongs of %d longs: %g seconds, %g GB/s, %g message/s\n",parts,count,elapsed,bw,rate);
+        fflush(stdout);
+      }
+
+      if (mem == 'd') {
+        CHECK(hipMemcpy(hpong,pong,bytes,hipMemcpyDeviceToHost));
+        for (int i = 0; i < end; i++) if (hping[i] != hpong[i]) MPI_Abort(MPI_COMM_WORLD,i);
+      } else {
+        for (int i = 0; i < end; i++) if (ping[i] != pong[i]) MPI_Abort(MPI_COMM_WORLD,i);
+      }
+      if (elapsed > timeout) break;
+    }
   }
   MPI_Finalize();
   return 0;
--- make.sh
+++ make.sh
@@ -2,4 +2,4 @@
 module load rocm
 module -t list
 set -x
-hipcc -g -O3 --offload-arch=gfx90a -I${CRAY_MPICH_DIR}/include -o pairs main.cc -L${CRAY_MPICH_DIR}/lib -lmpi ${PE_MPICH_GTL_DIR_amd_gfx908} ${PE_MPICH_GTL_LIBS_amd_gfx908}
+hipcc -ggdb -O --offload-arch=gfx90a -I${CRAY_MPICH_DIR}/include -o pairs main.cc -L${CRAY_MPICH_DIR}/lib -lmpi ${PE_MPICH_GTL_DIR_amd_gfx908} ${PE_MPICH_GTL_LIBS_amd_gfx908}
