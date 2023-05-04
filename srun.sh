0378dfa0d1fcfedc2c05b513982cfa9b2f12bcfa
Trey White <trey.white@hpe.com>
2023-05-03 20:05:36 -0400

    Added srun.sh.

--- srun.sh
+++ srun.sh
@@ -6,7 +6,7 @@ set -x
 export MPICH_GPU_SUPPORT_ENABLED=1
 export MPICH_OFI_NIC_POLICY=GPU
 TASKS=$1
+NODES=$2
 shift
-NODES=$1
 shift
-srun -n $TASKS -N $NODES --exclusive --gpus-per-task=1 --gpu-bind=closest -t 5:00 ./pairs $@ 
+srun -n $TASKS -N $NODES --exclusive --gpus-per-task=1 --gpu-bind=closest -t 5:00 ./pairs $@
